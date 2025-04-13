94152fac4c4f47254f7d3e7edd191d08ab1374c9
Gashan56 <gashanplay2020@gmail.com>
2025-04-13 17:37:11 +0300

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -2,13 +2,8 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return jsonify({"status": "OK", "message": "Hello from Python API!"})
-
 @app.route("/api/data")
 def get_data():
     return jsonify({"data": [1, 2, 3]})
 
-# Важно: Vercel требует явного экспорта приложения
-handler = app
+handler = app  # Важно!
